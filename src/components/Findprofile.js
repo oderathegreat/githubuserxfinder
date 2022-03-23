d47cbac3107df6bbdaf43cafc046be5885fbb2ce
oderathegreat <oderathegreat@gmail.com>
2022-03-23 08:00:20 +0300

    paginate page results

--- src/components/Findprofile.js
+++ src/components/Findprofile.js
@@ -34,6 +34,42 @@ function Findprofile() {
 
     }
 
+    //paginate page results
+     const paginate = (value) =>
+        {
+            if (value == 'next') {
+                setPage(page + 1)
+                const url = 'https://api.github.com/search/users?q=' + userName + '&&page=' + page + '&per_page=15'
+                fetch(url)
+                    .then((response) => response.json())
+                    .then((data) =>
+                    {
+                        console.log(data)
+                        setResults(data.items);
+                        setTotals(data.total_count);
+                    })
+                console.log(page)
+            } else { 
+                if (page > 1) {
+                    setPage(page - 1)
+                    const url = 'https://api.github.com/search/users?q=' + userName + '&&page=' + page + '&per_page=15'
+                    fetch(url)
+                        .then((response) => response.json())
+                        .then((data) =>
+                        {
+                            console.log(data)
+                            setResults(data.items);
+                            setTotals(data.total_count);
+                        })
+                } else { 
+                    alert('no page')
+                }
+            }
+    
+            
+    
+        }
+    
 
 
 
